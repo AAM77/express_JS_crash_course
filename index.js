3f1cb690fbc0edf5710cccb7420616c96d287a23
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-11 14:52:46 -0400

    installed moment. created a logger that displays the url and a timestamp (using moment). It runs each time we make a request to the api.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members.js')
+const members = require('./Members.js');
+const logger = require('./middleware/logger.js');
 
 const app = express();
+
+
+// INITIALIZE MIDDLEWARE
+app.use(logger);
 
 // GETS ALL MEMBERS
 app.get('/api/members', (req, res) => res.json(members));
