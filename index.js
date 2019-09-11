4319d4933be3a018dddf94adafbaf6ebb194946b
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-11 14:28:44 -0400

    refactored index.js to create routes for & load all static pages in public. Created a css folder with a style.css & basic styling. Added the link to the css file inside the html files.

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+// SET A STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
