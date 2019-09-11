a2d6d542930ddd404538034f50ec16297e2688b6
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-11 15:40:57 -0400

    added error checking to the single member request.

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ app.get('/api/members', (req, res) => res.json(members));
 
 // GET SINGLE MEMBER
 app.get('/api/members/:id', (req, res) => {
-  res.json(members.filter( member => member.id === parseInt(req.params.id)));
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 // SET A STATIC FOLDER
