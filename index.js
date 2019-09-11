28c51ec606a8bb3cb4fad2a735c3cb649e0fc75f
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-11 15:56:26 -0400

    Cleaned up index.js by moving routes to its own folder.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members.js');
 const logger = require('./middleware/logger.js');
 
 const app = express();
@@ -9,22 +8,11 @@ const app = express();
 // INITIALIZE MIDDLEWARE
 //app.use(logger);
 
-// GETS ALL MEMBERS
-app.get('/api/members', (req, res) => res.json(members));
-
-// GET SINGLE MEMBER
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }
-});
 
 // SET A STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
